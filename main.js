48979ebea39d8c475add8e9a38dca4646e8317ee
Dana <danamudrova@gmail.com>
2022-02-10 20:47:25 +0100

    ukol

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // tady je místo pro náš program
 
-document.querySelector("#vysledek").innerHTML = secti(4, 5);
+
 
 /**
  * Sečte dvě čísla na vstupu a vrátí výsledek
@@ -12,10 +12,26 @@ function secti(a, b) {
   let c = a + b;
   return c;
 }
+'ukol 1'
+function changeColor(){
+document.querySelector('.ctverecek').style.backgroundColor = 'green';
+}
+
+'ukol2'
+function display () {
+document.querySelector('#vysledek').innerHTML = secti(4,5);
+ 
+
+}
 
 /**
  * Upozorní uživatele při spuštění.
  */
+'ukol 3'
 function upozorni() {
   alert("Gratulace, právě jsi spustila tuto funkci!");
+  console.log("Gratulace, právě jsi spustila tuto funkci!");
+  document.querySelector('.ctverecek').innerHTML  = "Gratulace, právě jsi spustila tuto funkci!";
+  
 }
+
